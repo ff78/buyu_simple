ee1e98afe88624ef155b066225b121891eaeeda1
ff78 <58469983@qq.com>
2016-07-01 09:56:15 +0800

    del useless code

--- frameworks/runtime-src/Classes/server/AndroidHelper.cpp
+++ frameworks/runtime-src/Classes/server/AndroidHelper.cpp
@@ -14,36 +14,7 @@
 USING_NS_CC;
 
 extern "C"
-{
-    bool getJniMethod(jobject& jobj, JniMethodInfo &minfo, const char* sFunction, const char* sSignature)
-    {
-        if (!JniHelper::getStaticMethodInfo(minfo,JNI_CLASS_NAME,"getInstance","()Ljava/lang/Object;"))
-            return false;
-        jobj = minfo.env->CallStaticObjectMethod(minfo.classID, minfo.methodID);
-        log("call static");
-        return JniHelper::getMethodInfo(minfo, JNI_CLASS_NAME, sFunction, sSignature);
-    }
-
-    char* jstringTostring(JNIEnv* env, jstring jstr)
-    {
-        char* rtn = NULL;
-        jclass clsstring = env->FindClass("java/lang/String");
-        jstring strencode = env->NewStringUTF("utf-8");
-        jmethodID mid = env->GetMethodID(clsstring, "getBytes", "(Ljava/lang/String;)[B");
-        jbyteArray barr= (jbyteArray)env->CallObjectMethod(jstr, mid, strencode);
-        jsize alen = env->GetArrayLength(barr);
-        jbyte* ba = env->GetByteArrayElements(barr, JNI_FALSE);
-        if (alen > 0)
-        {
-            rtn = (char*)malloc(alen + 1);
-            
-            memcpy(rtn, ba, alen);
-            rtn[alen] = 0;
-        }
-        env->ReleaseByteArrayElements(barr, ba, 0);
-        return rtn;
-    }
-    
+{    
     void Java_org_cocos2dx_lua_AndroidHelper_OnPayProcessResult(JNIEnv *env, jobject thiz, int orderId, int errNo)
     {
 //        log("back to c++ result!");
